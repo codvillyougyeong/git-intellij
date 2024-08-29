4250e8fedfd32920019038b945aea37d7707bd28
Youniejang <younie.jang@gmail.com>
2024-08-29 14:23:38 +0900

    새로운 데이터 추가

---
+++ src/main/java/org/example/NewData.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class NewData {
+
+  private String id;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+
+}
